84be831796c377061a9b84d5f879781263464731
adaau <ada.au.cheng@gmail.com>
2015-11-19 22:18:20 +0800

    message not showing in div

--- app.js
+++ app.js
@@ -3,13 +3,31 @@ var app = express();
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 // Need to connect the socket to the http server
+var server = require('http').createServer(app)
+var port = process.env.PORT || 3000;
+var router = express.Router();
 
 
 app.set("views", __dirname + "/views");
-app.use(logger('dev'));
 app.engine('ejs', require('ejs').renderFile);
+app.use(logger('dev'));
 app.set('view engine', 'ejs');
 app.use(express.static("public", __dirname + "/public"));
 app.use(bodyParser.json());
 
 // Write your code here
+
+app.get('/', function(req, res) {
+  res.render('index');
+})
+
+
+server.listen(port);
+console.log('Server started on ' + port);
+var io = require('socket.io')(server);
+
+io.on('connect', function(socket){
+  socket.on('send', function(data) {
+    socket.emit('message', data);
+  });
+});
