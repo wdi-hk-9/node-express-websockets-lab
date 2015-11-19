d0e8935c668dd27654a00cf0726da2401634a4db
Philippa Johnstone <pecj@hotmail.co.uk>
2015-11-19 23:08:44 +0800

    SocketIo integrated and emitting events. Can broadcast 'message text' to client but not 'name'. Can console log both name and message text.

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+var logger = require("morgan");
 var express = require("express");
 var app = express();
-var logger = require("morgan");
 var bodyParser = require("body-parser");
-// Need to connect the socket to the http server
+var port = process.env.PORT || 3000;
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
+
+app.get('/', function(req, res) {
+  res.render('index.ejs');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('text', function (txt){
+    io.emit('text', txt);
+    console.log('text: ' + txt)
+  });
+  socket.on('name', function (n){
+    io.emit('name', n);
+    console.log('name: ' + n)
+  });
+});
+
+server.listen(port);
+console.log('Server started on ' + port);
+
+
+// var router = express.Router();
 
 app.set("views", __dirname + "/views");
 app.use(logger('dev'));
@@ -12,4 +36,5 @@ app.set('view engine', 'ejs');
 app.use(express.static("public", __dirname + "/public"));
 app.use(bodyParser.json());
 
-// Write your code here
+
+
