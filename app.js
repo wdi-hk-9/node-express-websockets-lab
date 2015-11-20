4def864854e8e30613291518da41a75768736cb1
“Ian <ianyhkwok@hotmail.com>
2015-11-20 10:39:48 +0800

    Ajax completed

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var express = require("express");
-var app = express();
-var logger = require("morgan");
-var bodyParser = require("body-parser");
+var express        = require("express");
+var app            = express();
+var expressLayouts = require('express-ejs-layouts')
+var logger         = require("morgan");
+var bodyParser     = require("body-parser");
+var port           = process.env.PORT || 3000;
+var router         = express.Router();
+
 // Need to connect the socket to the http server
+var server  = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 
 app.set("views", __dirname + "/views");
@@ -13,3 +19,19 @@ app.use(express.static("public", __dirname + "/public"));
 app.use(bodyParser.json());
 
 // Write your code here
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.post('/message', function(req, res) {
+  var message = req.body.message;
+  var name = req.body.name;
+});
+
+// validations
+// send a new messsage to all clients
+// repsond to post requrest
+
+console.log('Server started on ' + port);
+
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,25 @@
 // Write the front end javascript here
+$( document ).ready(function() {
+
+  $('#messageForm').on('click', function(e){
+    e.preventdefault;
+    console.log('clicked');
+
+    var name    = $('#name').val;
+    var message = $('#message').val;
+
+    console.log(name)
+
+
+    $.ajax({
+      url:          '/message',
+      type:         'POST',
+      contentType:  'application/json',
+      dataType:     'json',
+      data:         {name: name, message: message}
+    });
+
+  })
+
+
+})
