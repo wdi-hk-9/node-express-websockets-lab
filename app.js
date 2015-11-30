6c87829a019e4ef8d09debf1c7530020e47df5b1
“Ian <ianyhkwok@hotmail.com>
2015-11-30 09:25:02 +0800

    Completed

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ var logger         = require("morgan");
 var bodyParser     = require("body-parser");
 var port           = process.env.PORT || 3000;
 var router         = express.Router();
-
 // Need to connect the socket to the http server
 var server  = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+var io      = require('socket.io').listen(server);
 
 
 app.set("views", __dirname + "/views");
@@ -18,20 +17,27 @@ app.set('view engine', 'ejs');
 app.use(express.static("public", __dirname + "/public"));
 app.use(bodyParser.json());
 
-// Write your code here
 
+
+// LANDING PAGE
 app.get('/', function(req, res) {
   res.render('index');
 });
 
+// RECEIVE MESSAGE
 app.post('/message', function(req, res) {
   var message = req.body.message;
   var name = req.body.name;
+
+// send a new messsage to all clients side
+io.sockets.emit('IncomingMessage' , {name: name, message: message})
+
+// repsond to post requrest
+var response = {message: "We got your message"}
+res.status(200).json(repsonse)
+
 });
 
-// validations
-// send a new messsage to all clients
-// repsond to post requrest
+server.listen(3000);
 
 console.log('Server started on ' + port);
-
--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,30 @@
-// Write the front end javascript here
 $( document ).ready(function() {
 
-  $('#messageForm').on('click', function(e){
-    e.preventdefault;
-    console.log('clicked');
+  var base_url = document.domain;
+  var socket   = io.connect(base_url);
 
-    var name    = $('#name').val;
-    var message = $('#message').val;
+  $('#messageForm').on('submit', function(e){
+    e.preventDefault();
 
-    console.log(name)
+    var name    = $('#name').val();
+    var message = $('#outgoingMessage').val();
 
-
+    var data = {name: name, message: message};
     $.ajax({
       url:          '/message',
       type:         'POST',
       contentType:  'application/json',
       dataType:     'json',
-      data:         {name: name, message: message}
-    });
-
+      data:          JSON.stringify(data)
+    }).done(function(data){
+      console.log(data);
+    })
   })
 
+  //listening to incoming message
+  socket.on('IncomingMessage', function(data){
+    console.log(data)
+      $('#messages').prepend('<br>' + data.name + '</b><br/>' + data.message + '<hr / >');
+  })
 
 })
